f845d418f510e8bb9bccebd164b3be1d05d2721d
Sumit-Sharma-01 <sumitsharma6389@gmail.com>
2025-05-08 20:58:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const express= require("express");
+const path = require("path");
+const app= express();
+const userRoute=require("./routes/user")
+const blogRoute=require("./routes/blog")
+const mongoose=require("mongoose")
+const cookieParser=require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+const Blog=require("./models/blog")
+
+const PORT= process.env.PORT|| 8000;
+
+// app.use(express.json());
+mongoose
+.connect(process.env.MONGO_URL)
+.then((e)=>{
+    console.log("MongoDB connected");
+    
+})
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(checkForAuthenticationCookie("token"))
+
+app.use(express.static(path.resolve("./public")));
+
+
+
+
+
+app.get("/", async (req,res)=>{
+    const allBlogs= await Blog.find({});
+    res.render("home.ejs",{
+        user:req.user,
+        blogs:allBlogs  
+    });
+    
+})
+
+app.use("/user",userRoute);
+app.use("/blog",blogRoute);
+
+
+
+
+app.listen(PORT,()=>{
+    console.log("Server Started");
+    
+})
